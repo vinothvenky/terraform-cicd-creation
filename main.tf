b42d9f9c7d809ac08aae5b6f1e065d90379c6466
vinothvenky <vinothvenky93@gmail.com>
2024-02-28 07:30:36 +0530

    modified the main.tf file

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  region = "us-east-1"  
+  region = "us-west-1"  
 }
 
 resource "aws_instance" "ec2-instance" {
-  ami           = "ami-0c7217cdde317cfec"  
+  ami           = "ami-0ce2cb35386fc22e9"  
   instance_type = "t2.micro"                
   key_name      = "jenkins-key"      
 
   tags = {
-    Name = "demo-instance"  
+    Name = "demo-test-instance"  
   }
 }
