f9ce7c10e8b6cd3a277d58213379a804c5a9052e
vinothvenky <vinothvenky93@gmail.com>
2024-02-28 07:00:14 +0530

    pushing terraform code to git repo

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"  
+}
+
+resource "aws_instance" "ec2-instance" {
+  ami           = "ami-0c7217cdde317cfec"  
+  instance_type = "t2.micro"                
+  key_name      = "jenkins-key"      
+
+  tags = {
+    Name = "demo-instance"  
+  }
+}
