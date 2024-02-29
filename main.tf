5b589cfd783b84d5d18f68fb734eb61651c8fb45
vinothvenky <98956763+vinothvenky@users.noreply.github.com>
2024-02-29 08:15:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,10 @@ provider "aws" {
   region = "us-east-1"  
 }
 
-resource "aws_instance" "ec2-instance" {
+resource "aws_instance" "demo-ubuntu" {
   ami           = "ami-0c7217cdde317cfec"  
   instance_type = "t2.micro"                
-  key_name      = "jenkins-key"      
-
+ 
   tags = {
     Name = "demo-test-instance"  
   }
